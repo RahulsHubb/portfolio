ef6d4bdedc12499da54ae681f0587e52f814e276
RahulsHubb <dewalsuccess@gmail.com>
2025-09-15 18:05:26 +0530

    implemented tailwind css successfully

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,14 @@ const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+    <div className="flex h-screen items-center justify-center bg-red-500 text-white text-4xl font-bold">
+      Tailwind v4 Works 🎉
+    </div>
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind v4 is working! 🎉
+      </h1>
+    </div>
     </div>
   )
 }
